0b640158b037086d595b01c2336a8d36dfb68e95
ye1143850302 <1143850302@qq.com>
2020-02-14 22:12:49 +0800

    仿写京东首页

---
+++ js/index.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', function() {
+    //调用swiper
+    var swiper = new Swiper('.swiper-container', {
+        pagination: {
+            el: '.swiper-pagination',
+        },
+    });
+})
